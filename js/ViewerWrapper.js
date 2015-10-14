d5e9e5bbf4bb280f0ef350100211816230ea572e
Anthony Gross <agross@pirineos.local>
2015-10-14 21:30:30 +0200

    ViewerWrapper on going

--- js/ViewerWrapper.js
+++ js/ViewerWrapper.js
@@ -1,6 +1,8 @@
 function ViewerWrapper() {
-    var listeners = new KastListener();
-
+    var listeners   = new KastListener();
+    var _this       = this;
+    var _elm        = null;
+    
     var options = {
         id              : null,
         scale           : 1,
@@ -11,13 +13,60 @@ function ViewerWrapper() {
     };
 
     var construct = function () {
+        _elm = $(".kast");
         var id = Math.random();
-        $('.kast-viewer').attr('id', 'kast-' + id);
+        _elm.find('canvas').attr('id', 'kast-' + id);
         options.id = 'kast-' + id;
-        render();
+        
+        _this.refreshToolbar();
+        _this.render();
     };
 
-    var render = function () {
+    this.refreshToolbar = function(){
+        var btnZoomIn   = $('<li/>').html("+").on("click", function(){
+            _this.zoomIn();
+        });
+        var btnZoomOut  = $('<li/>').html("-").on("click", function(){
+            _this.zoomOut();
+        });
+        var btnPageNext = $('<li/>').html(">").on("click", function(){
+            _this.nextPage();
+        });
+        var btnPagePrev = $('<li/>').html("<").on("click", function(){
+            _this.prevPage();
+        });
+        
+        var ul_content  = $('<ul/>')
+                .append(btnPageNext)
+                .append(btnPagePrev)
+                .append(btnZoomIn)
+                .append(btnZoomOut);
+        _elm.find('.kast-viewer-toolbar').append(ul_content);
+    };
+    
+    this.nextPage = function(){
+        options.page = options.page+1;
+        firePageNext();
+        _this.render();
+    };
+    this.prevPage = function(){
+        options.page = options.page-1;
+        firePagePrev();
+        _this.render();
+    };
+    this.zoomIn = function(){
+        options.scale = options.scale+.1;
+        fireZoomIn();
+        _this.render();
+    };
+    this.zoomOut = function(){
+        options.scale = options.scale-.1;
+        fireZoomOut();
+        _this.render();
+    };
+    
+    
+    this.render = function () {
         PDFJS.getDocument(options.file).then(function (pdf) {
             // Using promise to fetch the page
             pdf.getPage(options.page).then(function (page) {
