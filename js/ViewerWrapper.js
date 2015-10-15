ff5e09bee1714857e718272e4bcbc71c55a9ae09
Anthony Gross <agross@pirineos.local>
2015-10-15 15:37:51 +0200

    design done

--- js/ViewerWrapper.js
+++ js/ViewerWrapper.js
@@ -3,14 +3,17 @@ function ViewerWrapper() {
     var _this       = this;
     var _elm        = null;
     
+    var width       = 780;
+    var height      = Math.round(width/1.33);
+    
     var options = {
         id              : null,
         scale           : 1,
         page            : 1,
         num_pages       : null,
-        canvas_height   : 744,
-        canvas_width    : 560,
-        file            : 'fixtures/openedx.pdf'
+        canvas_height   : height,
+        canvas_width    : width,
+        file            : 'fixtures/openedx_landscape.pdf'
     };
 
     var construct = function () {
@@ -24,28 +27,27 @@ function ViewerWrapper() {
     };
 
     this.refreshToolbar = function(){
-        var btnZoomIn   = $('<li/>').addClass("mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect").html('<i class="material-icons">zoom_in</i>').on("click", function(){
+        var btnZoomIn   = $('<button/>').addClass("mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect").html('<i class="material-icons">zoom_in</i>').on("click", function(){
             _this.zoomIn();
         }).attr("id", Math.random());
-        var btnZoomInTooltip = $('<li/>').addClass("mdl-tooltip").html("Zoom In").attr("for", btnZoomIn.attr('id'));
+        var btnZoomInTooltip = $('<div/>').addClass("mdl-tooltip").html("Zoom In").attr("for", btnZoomIn.attr('id'));
         
-        var btnZoomOut  = $('<li/>').addClass("mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect").html('<i class="material-icons">zoom_out</i>').on("click", function(){
+        var btnZoomOut  = $('<button/>').addClass("mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect").html('<i class="material-icons">zoom_out</i>').on("click", function(){
             _this.zoomOut();
         }).attr("id", Math.random());        
-        var btnZoomOutTooltip = $('<li/>').addClass("mdl-tooltip").html("Zoom out").attr("for", btnZoomOut.attr('id'));
+        var btnZoomOutTooltip = $('<div/>').addClass("mdl-tooltip").html("Zoom out").attr("for", btnZoomOut.attr('id'));
 
-        var btnPageNext = $('<li/>').addClass("mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect").html('<i class="material-icons">fast_forward</i>').on("click", function(){
+        var btnPageNext = $('<button/>').addClass("mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect").html('<i class="material-icons">fast_forward</i>').on("click", function(){
             _this.nextPage();
         }).attr("id", Math.random());
-        var btnPageNextTooltip = $('<li/>').addClass("mdl-tooltip").html("Next page").attr("for", btnPageNext.attr('id'));
+        var btnPageNextTooltip = $('<div/>').addClass("mdl-tooltip").html("Next page").attr("for", btnPageNext.attr('id'));
 
-        var btnPagePrev = $('<li/>').addClass("mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect").html('<i class="material-icons">fast_rewind</i>').on("click", function(){
+        var btnPagePrev = $('<button/>').addClass("mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect").html('<i class="material-icons">fast_rewind</i>').on("click", function(){
             _this.prevPage();
         }).attr("id", Math.random());
-        var btnPagePrevTooltip = $('<li/>').addClass("mdl-tooltip").html("Previous page").attr("for", btnPagePrev.attr('id'));
+        var btnPagePrevTooltip = $('<div/>').addClass("mdl-tooltip").html("Previous page").attr("for", btnPagePrev.attr('id'));
         
-        var ul_content  = $('<ul/>')
-                .append(btnPagePrev)
+        _elm.find('.kast-viewer-toolbar').append(btnPagePrev)
                 .append(btnPagePrevTooltip)
                 .append(btnPageNext)
                 .append(btnPageNextTooltip)
@@ -53,7 +55,6 @@ function ViewerWrapper() {
                 .append(btnZoomInTooltip)
                 .append(btnZoomOut)
                 .append(btnZoomOutTooltip);
-        _elm.find('.kast-viewer-toolbar').append(ul_content);
     };
     
     this.nextPage = function(){
@@ -82,12 +83,12 @@ function ViewerWrapper() {
         }
     };
     this.zoomIn = function(){
-        options.scale = options.scale+.1;
+        options.scale = options.scale-.1;
         fireZoomIn();
         _this.render();
     };
     this.zoomOut = function(){
-        options.scale = options.scale-.1;
+        options.scale = options.scale+.1;
         fireZoomOut();
         _this.render();
     };
@@ -102,7 +103,6 @@ function ViewerWrapper() {
                 var scale           = options.scale;
                 options.num_pages   = pdf.numPages;
                 
-                var viewport    = page.getViewport(scale);
                 //
                 // Prepare canvas using PDF page dimensions
                 //
@@ -113,6 +113,8 @@ function ViewerWrapper() {
                 //
                 // Render PDF page into canvas context
                 //
+                console.log(-1*scale);
+                var viewport    = page.getViewport(canvas.width / page.getViewport(scale).width);
                 var renderContext = {
                     canvasContext: context,
                     viewport: viewport
